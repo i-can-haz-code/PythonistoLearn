c91a1b318eb16b4a006e1398651ce7cdbafb64bc
i-can-haz-code <icanhazcode@mailinator.com>
2016-01-17 12:26:59 -0500

    fixed decorator a bit.

--- requests_decorator.py
+++ requests_decorator.py
@@ -7,9 +7,13 @@ Make a simple decorator to log cookies if they exist.
 import functools
 import requests
 
+__author__ = 'u/i_can_haz_code'
+
 def decorator(func,*args,**kwargs):
+	""" Simple decorator to print cookies. Modify at will. """
 	@functools.wraps(func)
 	def inner(*args,**kwargs):
+		""" Uses functools.wraps to keep from showing this in the help(func) """
 		ret = func(*args,**kwargs)
 		try:
 			cookies = ret.cookies
@@ -21,6 +25,7 @@ def decorator(func,*args,**kwargs):
 
 @decorator
 def get_request(url):
+	""" Sends a get request, and returns what comes back. """
 	r = requests.get(url)
 	return(r)
 
