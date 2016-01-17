32f876857b612ff0343a062ecc34c8d8b04dcf72
i-can-haz-code <icanhazcode@mailinator.com>
2016-01-17 13:10:35 -0500

    allows for draws now...

--- rpc.py
+++ rpc.py
@@ -6,17 +6,20 @@ choices = ['rock','paper','scissors']
 beats = {'rock':'scissors','scissors':'paper','paper':'rock'}
 def user_choice():
 	""" Gets the user choice. If it is invalid. Raise a ValueError """
-	choice = input('Please choose one of the following {} {} {} :'.format(choices[0],choices[1],
+	choice = input('Please choose one of the following {} {} {} : '.format(choices[0],choices[1],
 		choices[2])).lower()
 	if choice in choices:
 		return (choice)
 	else:
+		print('invalid option selected. {} '.format(choice))
 		raise (ValueError)
 def computer_choice():
 	""" This function arguably only exists for readability later on... """
 	return(random.choice(choices))
 def compare(user,computer):
 	""" Figure out who wins. """
+	if user == computer:
+		print('Draw user chose: {} computer chose: {}'.format(user,computer))
 	if beats[computer] == user:
 		print('Computer wins {}  to {}'.format(computer,user))
 	if beats[user] == computer:
@@ -29,8 +32,8 @@ def game():
 			uc = user_choice()
 			canary = 1
 		except ValueError:
-			print('invalid choice.')
 			pass
-	compare(uc,computer_choice())
+	cc = computer_choice()
+	compare(uc,cc)
 if __name__ == '__main__':
 	game()
